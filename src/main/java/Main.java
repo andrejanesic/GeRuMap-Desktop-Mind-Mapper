4e63fbca7d73497bae4a4a18966b5507aa94609b
Andreja Nesic <office@andrejanesic.com>
2022-10-13 00:32:57 +0200

    Updated and tested working Docker config. Updated POM.

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Main!");
+        JFrame f = new JFrame();
+        f.setName("Main");
+        f.setSize(400, 500);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
