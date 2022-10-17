e2243b0acff7b6ccfa776a75f235a12ed5ecf254
Nesic Andreja <anesic3119rn@raf.edu.yu>
2022-10-17 20:27:12 +0200

    Added singleton init.

--- src/main/java/rs/edu/raf/dsw/rudok/app/AppCore.java
+++ src/main/java/rs/edu/raf/dsw/rudok/app/AppCore.java
@@ -2,9 +2,27 @@ package rs.edu.raf.dsw.rudok.app;
 
 import rs.edu.raf.dsw.rudok.app.core.ApplicationFramework;
 
+/**
+ * Main application class.
+ */
 public class AppCore extends ApplicationFramework {
+
+    private AppCore() {
+
+    }
 
     public static void main(String[] args) {
 
+    }
+
+    public static ApplicationFramework getInstance() {
+        return AppCoreSingleton.INSTANCE;
+    }
+
+    /**
+     * Singleton wrapper.
+     */
+    private static class AppCoreSingleton {
+        private static final AppCore INSTANCE = new AppCore();
     }
 }
