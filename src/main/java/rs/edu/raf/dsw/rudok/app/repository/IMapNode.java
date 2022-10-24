7a27fe20974bb2ccf5fbb5660ac0a1b272b98c5a
Andreja Nesic <office@andrejanesic.com>
2022-10-24 23:00:31 +0200

    Added IMapNode and IMapNodeComposite.

---
+++ src/main/java/rs/edu/raf/dsw/rudok/app/repository/IMapNode.java
@@ -0,0 +1,72 @@
+package rs.edu.raf.dsw.rudok.app.repository;
+
+import rs.edu.raf.dsw.rudok.app.observer.IMessage;
+import rs.edu.raf.dsw.rudok.app.observer.IPublisher;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Composite design pattern unit. Cannot have any children.
+ */
+public abstract class IMapNode extends IPublisher {
+
+    /**
+     * Set of parents.
+     */
+    public Set<IMapNodeComposite> parents = new HashSet<>();
+
+    public Set<IMapNodeComposite> getParents() {
+        return parents;
+    }
+
+    public void setParents(Set<IMapNodeComposite> parents) {
+        this.parents = parents;
+    }
+
+    /**
+     * Adds a parent to the node.
+     *
+     * @param parent Node to add as parent.
+     */
+    public void addParent(IMapNodeComposite parent) {
+        if (parent == null) return;
+        if (this.parents.contains(parent)) return;
+        this.parents.add(parent);
+
+        this.publish(new Message(Message.Type.PARENT_ADDED, this));
+    }
+
+    /**
+     * Removes a parent from the node.
+     *
+     * @param parent Node to remove from parents.
+     */
+    public void removeParent(IMapNodeComposite parent) {
+        if (parent == null) return;
+        if (!this.parents.contains(parent)) return;
+        this.parents.remove(parent);
+
+        this.publish(new Message(Message.Type.PARENT_REMOVED, this));
+    }
+
+    /**
+     * Message for publishing.
+     */
+    public static class Message extends IMessage<IMapNode.Message.Type, IMapNode> {
+
+        public Message(Type status, IMapNode data) {
+            super(status, data);
+        }
+
+        /**
+         * Types of message.
+         */
+        public enum Type {
+            // When a parent is added
+            PARENT_ADDED,
+            // When a parent is removed
+            PARENT_REMOVED,
+        }
+    }
+}
---
+++ src/main/java/rs/edu/raf/dsw/rudok/app/repository/IMapNodeComposite.java
@@ -0,0 +1,89 @@
+package rs.edu.raf.dsw.rudok.app.repository;
+
+import rs.edu.raf.dsw.rudok.app.observer.IMessage;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Composite design pattern unit. Allowed to have children.
+ */
+public abstract class IMapNodeComposite extends IMapNode {
+
+    /**
+     * Set of all children.
+     */
+    private Set<IMapNode> children = new HashSet<>();
+
+    public Set<IMapNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Set<IMapNode> children) {
+        this.children = children;
+    }
+
+    /**
+     * Adds a child to the node.
+     *
+     * @param child Child to be added.
+     */
+    public void addChild(IMapNode child) {
+        if (child == null) return;
+        if (this.children.contains(child)) return;
+        this.children.add(child);
+
+        this.publish(new Message(Message.Type.CHILD_ADDED, this));
+    }
+
+    /**
+     * Removes a child from the node.
+     *
+     * @param child Child to be removed.
+     */
+    public void removeChild(IMapNode child) {
+        if (child == null) return;
+        if (!this.children.contains(child)) return;
+        this.children.remove(child);
+
+        this.publish(new Message(Message.Type.CHILD_REMOVED, this));
+    }
+
+    @Override
+    public void receive(Object message) {
+
+        if (message instanceof IMapNode.Message) {
+
+            switch (((IMapNode.Message) message).getStatus()) {
+
+                case PARENT_ADDED:
+                    this.addChild(((IMapNode.Message) message).getData());
+                    return;
+
+                case PARENT_REMOVED:
+                    this.removeChild(((IMapNode.Message) message).getData());
+                    return;
+            }
+        }
+    }
+
+    /**
+     * Message for publishing.
+     */
+    public static class Message extends IMessage<IMapNodeComposite.Message.Type, IMapNodeComposite> {
+
+        public Message(Type status, IMapNodeComposite data) {
+            super(status, data);
+        }
+
+        /**
+         * Types of message.
+         */
+        public enum Type {
+            // When a parent is added
+            CHILD_ADDED,
+            // When a parent is removed
+            CHILD_REMOVED,
+        }
+    }
+}
