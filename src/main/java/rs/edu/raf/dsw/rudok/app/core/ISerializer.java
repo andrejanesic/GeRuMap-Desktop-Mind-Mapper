b57b97bf8d560a110c1261bc36d48d4d6e82cc1d
Andreja Nesic <office@andrejanesic.com>
2022-10-25 00:01:52 +0200

    Updated ISerializer with basic functionality.

--- src/main/java/rs/edu/raf/dsw/rudok/app/core/ISerializer.java
+++ src/main/java/rs/edu/raf/dsw/rudok/app/core/ISerializer.java
@@ -1,7 +1,24 @@
 package rs.edu.raf.dsw.rudok.app.core;
 
+import java.io.Serializable;
+
 /**
  * Serializer component specification.
  */
 public interface ISerializer {
+
+    /**
+     * Saves the serializable object.
+     *
+     * @param serializable Serializable object.
+     */
+    void save(Serializable serializable);
+
+    /**
+     * Loads a serializable object from the given path. In case of error returns null.
+     *
+     * @param path Path to load the object from.
+     * @return The serializable object or null if error.
+     */
+    Serializable load(String path);
 }
