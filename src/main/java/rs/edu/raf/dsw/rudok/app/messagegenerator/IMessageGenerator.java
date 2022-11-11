88e8342698e43ee85cc7cbe4a2d9a71b6c01453e
Andreja Nesic <office@andrejanesic.com>
2022-11-11 14:35:29 +0100

    Added StandardMessageGenerator, publisher to IMessageGenerator.

--- src/main/java/rs/edu/raf/dsw/rudok/app/messagegenerator/IMessageGenerator.java
+++ src/main/java/rs/edu/raf/dsw/rudok/app/messagegenerator/IMessageGenerator.java
@@ -1,5 +1,9 @@
 package rs.edu.raf.dsw.rudok.app.messagegenerator;
 
+import rs.edu.raf.dsw.rudok.app.observer.IMessage;
+import rs.edu.raf.dsw.rudok.app.observer.IMessageData;
+import rs.edu.raf.dsw.rudok.app.observer.IPublisher;
+
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
@@ -7,7 +11,7 @@ import java.text.SimpleDateFormat;
  * <h1>Message generator</h1>
  * Used for generating messages propagated throughout the system, such as logs, warnings, errors, etc.
  */
-public interface IMessageGenerator {
+public abstract class IMessageGenerator extends IPublisher {
 
     /**
      * Sends the message with the given content, type and timestamp.
@@ -16,7 +20,7 @@ public interface IMessageGenerator {
      * @param type      Message type.
      * @param timestamp Message timestamp.
      */
-    void send(String content, Type type, String timestamp);
+    public abstract void send(String content, Type type, String timestamp);
 
     /**
      * Sends the message with the given content and type.
@@ -24,7 +28,7 @@ public interface IMessageGenerator {
      * @param content Message content.
      * @param type    Message type.
      */
-    default void send(String content, Type type) {
+    public void send(String content, Type type) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Timestamp t = new Timestamp(System.currentTimeMillis());
         this.send(content, type, sdf.format(t));
@@ -35,7 +39,7 @@ public interface IMessageGenerator {
      *
      * @param content Message content.
      */
-    default void log(String content) {
+    public void log(String content) {
         this.send(content, Type.LOG);
     }
 
@@ -44,7 +48,7 @@ public interface IMessageGenerator {
      *
      * @param content Message content.
      */
-    default void warning(String content) {
+    public void warning(String content) {
         this.send(content, Type.WARNING);
     }
 
@@ -53,13 +57,56 @@ public interface IMessageGenerator {
      *
      * @param content Message content.
      */
-    default void error(String content) {
+    public void error(String content) {
         this.send(content, Type.ERROR);
     }
 
-    enum Type {
+    /**
+     * Message types.
+     */
+    public enum Type {
         LOG,
         WARNING,
         ERROR,
+    }
+
+    /**
+     * Message wrapper for {@link IMessage} observer data.
+     */
+    public static class Message extends IMessage<Message.Type, Message.MessageData> {
+
+        public Message(Message.Type status, Message.MessageData data) {
+            super(status, data);
+        }
+
+        public enum Type {
+            Message,
+        }
+
+        public static class MessageData extends IMessageData<IMessageGenerator> {
+
+            private final IMessageGenerator.Type type;
+            private final String content;
+            private final String timestamp;
+
+            public MessageData(IMessageGenerator sender, IMessageGenerator.Type type, String content, String timestamp) {
+                super(sender);
+                this.type = type;
+                this.content = content;
+                this.timestamp = timestamp;
+            }
+
+            public IMessageGenerator.Type getType() {
+                return type;
+            }
+
+            public String getContent() {
+                return content;
+            }
+
+            public String getTimestamp() {
+                return timestamp;
+            }
+        }
     }
 }
---
+++ src/main/java/rs/edu/raf/dsw/rudok/app/messagegenerator/standard/StandardMessageGenerator.java
@@ -0,0 +1,17 @@
+package rs.edu.raf.dsw.rudok.app.messagegenerator.standard;
+
+import rs.edu.raf.dsw.rudok.app.messagegenerator.IMessageGenerator;
+
+/**
+ * <h1>Standard message generator</h1>
+ * Standard implementation of the {@link IMessageGenerator} service.
+ */
+public class StandardMessageGenerator extends IMessageGenerator {
+
+    @Override
+    public void send(String content, Type type, String timestamp) {
+        this.publish(new Message(Message.Type.Message, new Message.MessageData(
+                this, type, content, timestamp
+        )));
+    }
+}
