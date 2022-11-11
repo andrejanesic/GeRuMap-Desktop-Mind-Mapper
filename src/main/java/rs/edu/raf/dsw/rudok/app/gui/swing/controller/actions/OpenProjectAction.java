c43fa1a3fbde6d7cd337031d17659a390b6f87c1
Andreja Nesic <office@andrejanesic.com>
2022-11-11 13:39:01 +0100

    Finished OpenProjectAction.

--- src/main/java/rs/edu/raf/dsw/rudok/app/gui/swing/controller/actions/OpenProjectAction.java
+++ src/main/java/rs/edu/raf/dsw/rudok/app/gui/swing/controller/actions/OpenProjectAction.java
@@ -1,6 +1,13 @@
 package rs.edu.raf.dsw.rudok.app.gui.swing.controller.actions;
 
+import rs.edu.raf.dsw.rudok.app.AppCore;
+import rs.edu.raf.dsw.rudok.app.gui.swing.view.MainFrame;
+import rs.edu.raf.dsw.rudok.app.repository.Project;
+import rs.edu.raf.dsw.rudok.app.repository.ProjectExplorer;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
 
 /**
  * Opens a new {@link Project} from the given path string.
@@ -9,7 +16,17 @@ public class OpenProjectAction extends IAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JFileChooser fc = new JFileChooser();
+        int ret = fc.showOpenDialog(MainFrame.getInstance());
+        ProjectExplorer parent = AppCore.getInstance().getProjectExplorer();
 
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            Project child = AppCore.getInstance().getFileSystem().loadProject(file.getAbsolutePath());
+            if (child != null) {
+                parent.addChild(child);
+            }
+        }
     }
 
 }
