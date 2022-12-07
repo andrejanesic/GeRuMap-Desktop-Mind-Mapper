21df50f3e8382f881a58dbc53873178df0547ec1
Andreja Nesic <office@andrejanesic.com>
2022-12-07 18:01:28 +0100

    Removed accidentally commited files.

--- src/main/java/rs/edu/raf/dsw/rudok/app/gui/swing/mindmappanel/mapview/diagramview/view/IDiagramView.java
+++
@@ -1,9 +0,0 @@
-package rs.edu.raf.dsw.rudok.app.gui.swing.mindmappanel.mapview.diagramview.view;
-
-import javax.swing.*;
-
-/**
- * Prints a visual diagram with the contents of the {@link rs.edu.raf.dsw.rudok.app.repository.MindMap}.
- */
-public abstract class IDiagramView extends JInternalFrame {
-}
